c1805b21d311f5e3e0c75f4a34ab032df1a2c3a5
Courtney Hodge <yss2zv@virginia.edu>
2024-07-02 17:04:25 -0400

    adding M09

---
+++ hw/M09/book_lover_demo.py
@@ -0,0 +1,10 @@
+from booklover.booklover import BookLover
+
+reader = BookLover('Sarah','Sarah_reads@gmail.com', 'horror')
+
+reader.add_book('Diary of A Wimpy Kid', 5)
+
+print(reader.num_books_read)
+
+
+
